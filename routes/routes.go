6f50af8bf7a276f2937878183335b66822db88a1
mubashir01234 <omerkhan01234@gmail.com>
2023-04-20 23:14:20 +0500

    add heroku setup

--- routes/routes.go
+++ routes/routes.go
@@ -22,7 +22,6 @@ func Routes() *mux.Router {
 	user.HandleFunc("/update", middlewares.IsAuthorized(controllers.UpdateUser)).Methods("PUT")
 
 	// Content API routes
-
 	content := api.PathPrefix("/content").Subrouter()
 	content.HandleFunc("/post", middlewares.IsAuthorized(controllers.PostContent)).Methods("POST")
 	content.HandleFunc("/all", controllers.GetAllCreatorsContent).Methods("GET")
@@ -39,21 +38,5 @@ func Routes() *mux.Router {
 	creator.HandleFunc("/unfollow/{creator_id}", middlewares.IsAuthorized(controllers.UnfollowCreator)).Methods("DELETE")
 	creator.HandleFunc("/{user_id}/followers/", controllers.GetCreatorFollowers).Methods("GET")
 
-	// challenge.HandleFunc("/", middlewares.IsAuthorized(controllers.CreateChallenge)).Methods("POST")
-	// challenge.HandleFunc("/user/{username}", controllers.GetChallenges).Methods("GET")
-	// challenge.HandleFunc("/{id}", middlewares.IsAuthorized(controllers.GetChallenge)).Methods("GET")
-	// challenge.HandleFunc("/{id}", middlewares.IsAuthorized(controllers.UpdateChallenge)).Methods("PUT")
-	// challenge.HandleFunc("/{id}", middlewares.IsAuthorized(controllers.DeleteChallenge)).Methods("DELETE")
-	// challenge.HandleFunc("/{id}/join/", middlewares.IsAuthorized(controllers.JoinChallenge)).Methods("POST")
-	// challenge.HandleFunc("/{id}/unjoin/", middlewares.IsAuthorized(controllers.UnJoinChallenge)).Methods("POST")
-
-	// api.HandleFunc("/person", controllers.CreatePersonEndpoint).Methods("POST")
-	// api.HandleFunc("/people", middlewares.IsAuthorized(controllers.GetPeopleEndpoint)).Methods("GET")
-	// api.HandleFunc("/person/{id}", controllers.GetPersonEndpoint).Methods("GET")
-	// api.HandleFunc("/person/{id}", controllers.DeletePersonEndpoint).Methods("DELETE")
-	// api.HandleFunc("/person/{id}", controllers.UpdatePersonEndpoint).Methods("PUT")
-
-	// router.HandleFunc("/upload", controllers.UploadFileEndpoint).Methods("POST")
-	// router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./uploaded/"))))
 	return router
 }
